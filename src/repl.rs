6aa49611dbc706700cf6c3bde91a21b1d31e9288
Tsubaki Sakura <tsubaki.sakura.aegis@gmail.com>
2021-11-09 00:41:42 +0900

    表示の日本語化

--- src/repl.rs
+++ src/repl.rs
@@ -9,7 +9,35 @@ enum CmdLine {
     Help,
     Exit,
     Empty,
+    Verbose,
+    Simple,
     Eval(State),
+}
+
+struct ReplContext
+{
+    verbose : bool,
+}
+
+impl Action {
+    fn translate_ja(&self) -> &'static str {
+        match *self {
+            Action::CannotAction => "選択肢なし",
+            Action::BasicTouch => "加工",
+            Action::StandardTouch => "中級加工",
+            Action::PrudentTouch => "倹約加工",
+            Action::FocusedTouch => "注視加工",
+            Action::PreparatoryTouch => "下地加工",
+            Action::ByregotsBlessing => "ビエルゴの祝福",
+            Action::MastersMend => "マスターズメンド",
+            Action::Observe => "経過観察",
+            Action::WasteNot => "倹約",
+            Action::WasteNot2 => "長期倹約",
+            Action::GreatStrides => "グレートストライド",
+            Action::Innovation => "イノベーション",
+            Action::Manipulation => "マニピュレーション",
+        }
+    }
 }
 
 fn parse_eval( v:&[&str] ) -> Result<CmdLine,&'static str> {
@@ -48,6 +76,10 @@ fn parse_cmdline( line:&str ) -> Result<CmdLine,&'static str> {
             "eval" => parse_eval(&v[1..]),
             "?" => Ok(CmdLine::Help),
             "h" => Ok(CmdLine::Help),
+            "v" => Ok(CmdLine::Verbose),
+            "verbose" => Ok(CmdLine::Verbose),
+            "s" => Ok(CmdLine::Simple),
+            "simple" => Ok(CmdLine::Simple),
             "help" => Ok(CmdLine::Help),
             "quit" => Ok(CmdLine::Exit),
             "exit" => Ok(CmdLine::Exit),
@@ -59,7 +91,7 @@ fn parse_cmdline( line:&str ) -> Result<CmdLine,&'static str> {
     }
 }
 
-fn print_series( setting:&Setting, ta:&Table<Action>, initial_state:&State ) {
+fn print_series( ctx:&ReplContext, setting:&Setting, ta:&Table<Action>, initial_state:&State ) {
 
     if !ta.contains(initial_state) {
         println!("Out of bound(0<=cp<={} && 5<=durability<={} && durability%5==0)", setting.max_cp, setting.max_durability );
@@ -73,29 +105,49 @@ fn print_series( setting:&Setting, ta:&Table<Action>, initial_state:&State ) {
         let (ns,q) = s.run_action(&setting, &ta[s]);
         sum_q += q;
 
-        println!("{:?} {:?} -> {}(+{})",s,ta[s],sum_q,q);
+        if ctx.verbose {
+            println!("耐久:{:2} CP:{:3} マニピュ:{} イノベ:{} グレスト:{} 倹約:{} 加工:{} 経過観察:{} 品質:{:5}(+{:5}) {:10}",
+                s.durability,
+                s.cp,
+                s.buff.manipulation,
+                s.buff.innovation,
+                s.buff.great_strides,
+                s.buff.waste_not,
+                s.buff.basic_touch,
+                s.buff.observe,
+                sum_q,
+                q,
+                ta[s].translate_ja() );
+        }
+        else {
+            println!("{}", ta[s].translate_ja());
+        }
+
         s = ns;
     }
-    println!("{:?}", s);
 }
 
 fn print_help() {
     println!("Usage:");
     println!("  [CP] [durability]       print tactics");
     println!("  eval [CP] [durability]  print tactics(same as above)");
+    println!("  v, verbose              verbose mode" );
+    println!("  s, simple               simple mode" );
     println!("  ?, h, help              print help" );
     println!("  exit, quit              exit command" );
 }
 
-fn eval_line( setting:&Setting, ta:&Table<Action>, line:&str ) -> bool {
+fn eval_line( ctx:&mut ReplContext, setting:&Setting, ta:&Table<Action>, line:&str ) -> bool {
 
     let cmdline = parse_cmdline( &line );
 
     match cmdline {
         Ok(cmd) => match cmd {
-            CmdLine::Eval(s) => print_series(setting,ta,&s),
+            CmdLine::Eval(s) => print_series(ctx,setting,ta,&s),
             CmdLine::Help => print_help(),
             CmdLine::Empty => {},
+            CmdLine::Verbose => { *ctx = ReplContext{ verbose: true } },
+            CmdLine::Simple => { *ctx = ReplContext{ verbose: false } },
             CmdLine::Exit => return false,
         },
         Err(x) => {
@@ -108,13 +160,15 @@ fn eval_line( setting:&Setting, ta:&Table<Action>, line:&str ) -> bool {
 
 pub fn repl( setting:&Setting, ta:&Table<Action> ) {
 
+    let mut ctx = ReplContext { verbose: true };
+
     let mut rl = Editor::<()>::new();
     loop {
         let readline = rl.readline(">> ");
         match readline {
             Ok(line) => {
                 rl.add_history_entry(line.as_str());
-                if !eval_line( &setting, &ta, &line ) {
+                if !eval_line( &mut ctx, &setting, &ta, &line ) {
                     break;
                 }
             },
